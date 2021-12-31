30e075cd2ddba697724503ec21531dd9e7872d83
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-12-31 13:05:06 +0000

    add url for post_detail

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 urlpatterns = [
     # blank path to indicate default as home page
     # and render this class as a view using 'as_view()'
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    # 'slug' keyword matches 'slug' parameter in get method of PostDetail class
+    # path converter (slug) if matched will return a string, if not a 404 message
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
