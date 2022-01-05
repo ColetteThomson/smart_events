72b4c421246bb19d9ad1d44ad788074e4e8ce8a1
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-05 13:01:30 +0000

    initial event model commit

--- events/models.py
+++ events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+#  ERD model for class Event.
+class Event(models.Model):
+    event_name = models.CharField(max_length=120)
+    event_date = models.DateTimeField()
+    venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=60)
+    # set description field to optional
+    description = models.TextField(blank=True)
