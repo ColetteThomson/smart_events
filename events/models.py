67365f01bfba789f839f42c8c607cf2e8b11ad9b
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-06 11:57:03 +0000

    add Venue and Users classes

--- events/models.py
+++ events/models.py
@@ -2,16 +2,48 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-#  ERD model for class Event.
+
+# ERD model for class Venue
+class Venue(models.Model):
+    venue_name = models.CharField(max_length=120)
+    address = models.CharField(max_length=300)
+    contact_no = models.CharField(max_length=20)
+    website = models.URLField()
+    venue_email = models.EmailField()
+
+    # helper method for class Venue:
+    # returns a string representation of an object
+    def __str__(self):
+        return self.venue_name
+
+
+# ERD model for class Users
+class Users(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_email = models.EmailField()
+
+    # helper method for class Users:
+    # returns a string representation of an object
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+#  ERD model for class Event
 class Event(models.Model):
     event_name = models.CharField(max_length=120)
     event_date = models.DateTimeField()
-    venue = models.CharField(max_length=120)
+    # to allow a new event to be added without a venue assigned
+    # if one-to-many venue relationship is deleted so will all related records
+    venue = models.ForeignKey(Venue, blank=True, null=True,
+                              on_delete=models.CASCADE)
     manager = models.CharField(max_length=60)
-    # set description field to optional
+    # to allow an event to be saved without any attendees
+    attendees = models.ManyToManyField(Users, blank=True)
+    # to set description field to optional
     description = models.TextField(blank=True)
 
-# helper methods for class Event:
+# helper method for class Event:
 # returns a string representation of an object
     def __str__(self):
-       return self.event_name
+        return self.event_name
