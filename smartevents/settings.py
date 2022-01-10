175684256ba8acfabd91776b039c82ca99ad935e
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-10 10:26:44 +0000

    edit directory structure for templates

--- smartevents/settings.py
+++ smartevents/settings.py
@@ -95,7 +95,9 @@ ROOT_URLCONF = 'smartevents.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
