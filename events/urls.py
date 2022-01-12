fde878a5b04ae60c88aac077699354e0a143cb31
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-12 14:59:28 +0000

    add all events function

--- events/urls.py
+++ events/urls.py
@@ -4,16 +4,15 @@ from . import views
 urlpatterns = [
     # blank path to indicate default as home page
     # and render this class as a view using 'as_view()'
-    # path('', views.PostList.as_view(), name='home'),
+    
     # path converter if matched to return a string, if not a 404 message
 
     # capture url and pass to calendar view
     path('event_calendar', views.event_calendar, name='event_calendar'),
     path('<int:year>/<str:month>/', views.event_calendar, name="event_calendar"),
+    # path('events/', views.all_events, name='show_events'),
     
-    # path('<int:year>/<str:month>/', views.calendar, name='calendar'),
-
-    # regex comment...p96...
+        # regex comment...p96...
     # re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',
            #  views.calendar, name='calendar'),
 ]
--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic, View
 from django.http import HttpResponse
 from datetime import date
+from datetime import datetime
 import calendar
 from calendar import HTMLCalendar
 from .models import Event
@@ -11,69 +12,30 @@ def event_calendar(request, todays_date=date.today()):
     year = todays_date.year
     month = todays_date.strftime('%B')
     month_number = todays_date.month
-    # convert month from characters to number
-    # month_no = str(calendar.month_name).calendar(month)
-    # month_no = int(month_no)
-
+    
     # create calendar
     cal = HTMLCalendar().formatmonth(
         year,
         month_number)
+    
+    # get current year
+    now = datetime.now()
+    current_year = now.year
 
     return render(request,
-                'event_calendar.html', {
-                "name": name,
-                "year": year,
-                "month": month,
-                "month_no": month_number,
-                "cal": cal,
-                })
+                  'event_calendar.html', {
+                                          "name": name,
+                                          "year": year,
+                                          "month": month,
+                                          "month_no": month_number,
+                                          "cal": cal,
+                                          "current_year": current_year,
+                                        })
 
-# def calendar(request, year=date.today().year, month=date.today().month):
-    # convert year and month to integers
-    # year = int(year)
-    # month = int(month)
-    # if year < 1900 or year > 2099:
-    #     year = date.today().year
-    # month_name = calendar.month_name[month]
-    # title = "Smart Events Calendar - %s %s" % (month_name, year)
-    # # retrieve HTML formatted calendar
-    # cal = HTMLCalendar().formatmonth(year, month)
-    # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
-    
 
-# function: dynamic title showing current month
-# if year,month not passed to 'calendar' then will default to current month
-# def calendar(request, year=date.today().year, month=date.today().month):
-    # convert year and month to integers
-    # year = int(year)
-    # month = int(month)
-    # if year is less than 2000 or greater than 2099, set year to this year
-    # if year < 2000 or year > 2099: year = date.today().year
-    # month_name = calendar.month_name[month]
-    # title = "Smart Events Calendar - %s %s" % (month_name, year)
-    # retrieve HTML formatted calendar
-    # cal = HTMLCalendar().formatmonth(year, month)
-    # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
-
-    # t = date.today()
-    # month = date.strftime(t, '%b')
-    # year = t.year
-
-    # return render(
-     #    request,
-       #  "calendar.html",
-        # {
-          #  'title': title,
-           # 'cal': cal
-        #}
-    #)
-    
-
-#def all_events(request):
-   # event_list = Event.objects.all()
-    #return render(
-       # request,
-      #  "events/event_list.html",
-      #  {'event_list': event_list}
-  #  )
+# def all_events(request):
+#     event_list = Event.objects.all()
+#     return render(request,
+#         'events/event_list.html',
+#         {"event_list": event_list}
+#     )
