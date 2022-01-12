34782e4edb7e5d22f97031ae2ca7811ade470df6
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-12 12:55:57 +0000

    edit event views.py

--- events/urls.py
+++ events/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     # path converter if matched to return a string, if not a 404 message
 
     # capture url and pass to calendar view
-    path('calendar', views.calendar, name='calendar'),
+    path('event_calendar', views.event_calendar, name='event_calendar'),
+    path('<int:year>/<str:month>/', views.event_calendar, name="event_calendar"),
     
     # path('<int:year>/<str:month>/', views.calendar, name='calendar'),
 
     # regex comment...p96...
-    re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',
-            views.calendar, name='calendar'),
+    # re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',
+           #  views.calendar, name='calendar'),
 ]
--- events/views.py
+++ events/views.py
@@ -6,17 +6,40 @@ import calendar
 from calendar import HTMLCalendar
 from .models import Event
 
-def calendar(request, year=date.today().year, month=date.today().month):
+def event_calendar(request, todays_date=date.today()):
+    name = "Marty"
+    year = todays_date.year
+    month = todays_date.strftime('%B')
+    month_number = todays_date.month
+    # convert month from characters to number
+    # month_no = str(calendar.month_name).calendar(month)
+    # month_no = int(month_no)
+
+    # create calendar
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number)
+
+    return render(request,
+                'event_calendar.html', {
+                "name": name,
+                "year": year,
+                "month": month,
+                "month_no": month_number,
+                "cal": cal,
+                })
+
+# def calendar(request, year=date.today().year, month=date.today().month):
     # convert year and month to integers
-    year = int(year)
-    month = int(month)
-    if year < 1900 or year > 2099:
-        year = date.today().year
-    month_name = calendar.month_name[month]
-    title = "Smart Events Calendar - %s %s" % (month_name, year)
-    # retrieve HTML formatted calendar
-    cal = HTMLCalendar().formatmonth(year, month)
-    return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
+    # year = int(year)
+    # month = int(month)
+    # if year < 1900 or year > 2099:
+    #     year = date.today().year
+    # month_name = calendar.month_name[month]
+    # title = "Smart Events Calendar - %s %s" % (month_name, year)
+    # # retrieve HTML formatted calendar
+    # cal = HTMLCalendar().formatmonth(year, month)
+    # return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
     
 
 # function: dynamic title showing current month
