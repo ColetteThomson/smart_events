c85e168479fc40636aa938aae963223c61034a07
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-14 13:23:16 +0000

    edit admin panel layout

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Venue, User
+from .models import Event, Venue, SiteUser
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,15 +8,19 @@ from django_summernote.admin import SummernoteModelAdmin
 class VenueAdmin(admin.ModelAdmin):
     # customise list view of admin panel
     list_display = ('venue_name', 'contact_no', 'address', 'venue_email')
+    # order venues alphabetically (ascending)
+    ordering = ('venue_name',)
     # add search fields for venue name
     search_fields = ['venue_name']
 
 
 # decorator to register User admin class for admin site
-@admin.register(User)
+@admin.register(SiteUser)
 class UsersAdmin(admin.ModelAdmin):
     # customise list view of admin panel
     list_display = ('first_name', 'last_name', 'user_email')
+    # order users alphabetically by last name
+    ordering = ('last_name',)
     # add search fields for either user's last name or email
     search_fields = ['last_name', 'user_email']
     # add filter for user's last name or email
@@ -28,9 +32,11 @@ class UsersAdmin(admin.ModelAdmin):
 class EventAdmin(SummernoteModelAdmin):
     # customise list view of admin panel
     list_display = ('event_name', 'venue', 'event_date', 'manager')
+    # order events earliest to latest
+    ordering = ('event_date',)
     # add search fields for either event or date or manager
     search_fields = ['event_name', 'event_date', 'manager']
     # add filter for event date and manager
-    list_filter = ('event_date', 'manager')
+    list_filter = ('event_name', 'event_date', 'venue')
     # summernote formatting to be applied to event's 'description' field
     summernote_fields = ('description')
--- events/models.py
+++ events/models.py
@@ -7,9 +7,9 @@ from cloudinary.models import CloudinaryField
 class Venue(models.Model):
     venue_name = models.CharField(max_length=120)
     address = models.CharField(max_length=300)
-    contact_no = models.CharField(max_length=20)
-    website = models.URLField()
-    venue_email = models.EmailField()
+    contact_no = models.CharField(max_length=20, blank=True)
+    website = models.URLField(blank=True)
+    venue_email = models.EmailField(blank=True)
 
     # helper method for class Venue:
     # returns a string representation of an object
@@ -17,8 +17,8 @@ class Venue(models.Model):
         return self.venue_name
 
 
-# ERD model for class User
-class User(models.Model):
+# ERD model for class SiteUser
+class SiteUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     user_email = models.EmailField()
@@ -36,10 +36,11 @@ class Event(models.Model):
     # to allow a new event to be added without a venue assigned
     # if one-to-many venue relationship is deleted so will all related records
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    # sets manager field to null, should manager cease to be a user
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     # to allow an attendee to attend many events and
     # to allow an event to be saved without any attendees
-    attendees = models.ManyToManyField(User, blank=True)
+    attendees = models.ManyToManyField(SiteUser, blank=True)
     # to set description field to optional
     description = models.TextField(blank=True)
 
