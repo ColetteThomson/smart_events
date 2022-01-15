fc395d69f91a92f23417b471228f4e556f3a8abf
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-15 12:55:05 +0000

    edit add_venue views and html files

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # blank path to indicate default as home page
     # and render this class as a view using 'as_view()'
-    
+
     # path converter if matched to return a string, if not a 404 message
 
     # capture url and pass to calendar view
@@ -12,8 +12,5 @@ urlpatterns = [
     path('<int:year>/<str:month>/', views.event_calendar, name="event_calendar"),
     path('all_events', views.all_events, name='all_events'),
     path('add_venue', views.add_venue, name='add_venue'),
-    
-        # regex comment...p96...
-    # re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',
-           #  views.calendar, name='calendar'),
-]
+
+    ]
--- events/views.py
+++ events/views.py
@@ -10,36 +10,68 @@ from .models import Event
 from .forms import VenueForm
 
 
-
 def add_venue(request):
-    # if form not submitted previously - render empty form
-    submitted = False
-    # if user submitted a completed form (post)
-    if request.method == "POST":
-        # take whatever info entered and post to VenueForm
-        form = VenueForm(request.POST)
-        # determine if entered info is valid
-        if form.is_valid():
-            # if valid save to database
-            form.save()
-            # form is submitted as true
-            return HttpResponseRedirect('/add_venue?submitted=True')
-        # if user didn't complete form
-        else:
-            form = VenueForm
-            # check if user submitted form
-            if 'submitted' in request.GET:
-                submitted = True
+    # obtain all data posted from form
+    venue_form = VenueForm(data=request.POST)
 
-            return render(request,
-                          'add_venue.html', {
-                                          'form': form,
-                                          'submitted': submitted,
-                         })
+    # if venue form is valid (required fields completed)
+    if venue_form.is_valid():
+        # save to database
+        new_venue = venue_form.save(commit=False)
+        # then save the new venue
+        new_venue.save()
+        # form is submitted as true
+        # return HttpResponseRedirect('/add_venue?submitted=True')
+
+        return render(request,
+                      'add_venue.html', {
+                                    'venue_form': venue_form,
+                                    'submitted': True,
+                                    })
+
+    else:
+        venue_form = VenueForm()
+        # check if user submitted form
+        if 'submitted' in request.GET:
+            submitted = True
+
+        return render(request,
+                      'add_venue.html', {
+                                       'venue_form': venue_form,
+                                        })
+
+    
+
+# def add_venue(request):
+#     # if form not submitted previously - render empty form
+#     submitted = False
+#     # if form is valid (user completed required fields)
+#     if request.method == "POST":
+#         # take whatever info entered and post to VenueForm
+#         form = VenueForm(request.POST)
+#         # determine if entered info is valid
+#         if form.is_valid():
+#             # if valid save to database
+#             form.save()
+#             # form is submitted as true
+#             return HttpResponseRedirect('/add_venue?submitted=True')
+#         # if user didn't complete form
+#     else:
+#         form = VenueForm
+#         # check if user submitted form
+#         if 'submitted' in request.GET:
+#             submitted = True
+
+#         return render(request,
+#                       'add_venue.html', {
+#                                        'form': form,
+#                                        'submitted': submitted,
+#                                         })
+
 
 def all_events(request):
     event_list = Event.objects.all()
-           
+        
     return render(request,
                   'all_events.html', {
                                      "event_list": event_list,
