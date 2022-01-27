e6d4c4112cb6bb6b6a91257541613f82e9ea4823
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-01-27 19:49:04 +0000

    fix 'chat' app pages

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     # blank path to indicate default as home page
     # and render this class as a view using 'as_view()'
-    path('chat', views.PostList.as_view(), name='post_list'),
+    #path('chat/', views.PostList.as_view(), name='post_list'),
+    path('', views.PostList.as_view(), name='post_list'),
     # 'slug' keyword matches 'slug' parameter in get method of PostDetail class
     # path converter (slug) if matched to return a string, if not a 404 message
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
--- chat/views.py
+++ chat/views.py
@@ -7,10 +7,11 @@ from .models import Post
 from .forms import CommentForm
 
 # (generic.ListView):
-class PostList(View):
+class PostList(generic.ListView):
     model = Post
     # filter contents of post table by '1' (published) by descending date
     queryset = Post.objects.filter(status=1).order_by('-created_on')
+    print(queryset)
     # to render html view
     template_name = 'post_list.html'
 
