c9f491bbbd7dd7b9fa3083da6239926dbae250fb
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2022-02-19 20:02:41 +0000

    edit formatting

--- smartevents/settings.py
+++ smartevents/settings.py
@@ -135,8 +135,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation. \
-            UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
